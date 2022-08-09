782dd92ed8586026ef568221c3170816646f66f0
danilo.salve <danilo.salve@totvs.com.br>
2022-08-09 09:03:57 -0300

    feat: Implementação da visão utilizando o po-list-view na pagina de configurações

--- src/app/pages/my-settings/home-my-settings/home-my-settings.module.ts
+++ src/app/pages/my-settings/home-my-settings/home-my-settings.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@app/shared/shared.module';
 import { SettingsListComponent } from './settings-list/settings-list.component';
 import { SettingTableComponent } from './settings-list/setting-table/setting-table.component';
+import { SettingListViewComponent } from './settings-list/setting-list-view/setting-list-view.component';
 
 @NgModule({
-  declarations: [SettingsListComponent, SettingTableComponent],
+  declarations: [SettingsListComponent, SettingTableComponent, SettingListViewComponent],
   imports: [SharedModule]
 })
 export class HomeMySettingsModule { }
