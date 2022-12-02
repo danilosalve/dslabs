f00cf6d5ccffc82fc0a316fa5dbd6c8b7ef6787d
danilo.salve <danilo.salve@totvs.com.br>
2022-12-02 15:52:59 -0300

    feat: exportação de dados do cliente para excel

--- src/app/pages/my-customers/home-my-customers/customer-list/customer-list.component.ts
+++ src/app/pages/my-customers/home-my-customers/customer-list/customer-list.component.ts
@@ -1,37 +1,52 @@
 import { Component, Injector } from '@angular/core';
 import { Router } from '@angular/router';
 import { BaseResourceList } from '@app/shared/components/base/base-resource-list.component';
-import { PoPageAction, PoTableAction } from '@po-ui/ng-components';
+import { PoPageAction } from '@po-ui/ng-components';
+import * as XLSX from 'xlsx';
 import { CustomerService } from '../../shared/services/customer.service';
-import { DocumentPipe } from './../../../../shared/pipe/document.pipe';
 import { Customer } from './../../shared/interface/customer';
 
 @Component({
-  selector: 'app-customer-list',
-  templateUrl: './customer-list.component.html'
+    selector: 'app-customer-list',
+    templateUrl: './customer-list.component.html'
 })
 export class CustomerListComponent extends BaseResourceList<Customer> {
-  constructor(
-    protected customerService: CustomerService,
-    protected override injector: Injector,
-    protected router: Router
+    constructor(
+        protected customerService: CustomerService,
+        protected override injector: Injector,
+        protected router: Router
     ) {
-      super(injector, customerService);
+        super(injector, customerService);
     }
 
     handleSearch(resource: Customer[], search: string): Customer[] {
-      return resource.filter(customers => customers.id?.toString().includes(search) ||
-      customers.name
-        ?.toLowerCase()
-        .includes(search.toLowerCase()));
+        return resource.filter(
+            customers =>
+                customers.id?.toString().includes(search) ||
+                customers.name?.toLowerCase().includes(search.toLowerCase())
+        );
     }
 
     getActions(): PoPageAction[] {
-      return [];
+        return [
+            {
+                label: 'Exportar para Excel',
+                action: () => this.onExportToexcel(),
+                icon: 'po-icon-export',
+                type: 'default'
+            }
+        ];
     }
 
     onShowCustomer(customer: Customer): void {
-      this.isLoading = true;
-      this.router.navigate(['customers/view', customer.id]);
+        this.isLoading = true;
+        this.router.navigate(['customers/view', customer.id]);
+    }
+
+    onExportToexcel(): void {
+        const ws = XLSX.utils.json_to_sheet(this.items);
+        const wb: XLSX.WorkBook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+        XLSX.writeFile(wb, 'customers.xlsx');
     }
 }
